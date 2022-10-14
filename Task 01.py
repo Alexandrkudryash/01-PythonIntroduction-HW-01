3f25925a46281479aa45488bdb81cb7c90c162ca
Alexander <alexandrkudryash@mail.ru>
2022-10-14 20:36:57 +0300

    modified Task01 to shorter code

--- Task 01.py
+++ Task 01.py
@@ -10,19 +10,9 @@
 # - 1 -> нет
 
 a = int(input('введите день недели ') )
-if a == 1:
-    print(f'- {a} -> нет')
-elif a == 2:
-    print(f'- {a} -> нет')
-elif a == 3:
-    print(f'- {a} -> нет')
-elif a == 4:
-    print(f'- {a} -> нет')
-elif a == 5:
-    print(f'- {a} -> нет')
-elif a == 6:
-    print(f'- {a} -> да')
-elif a == 7:
-    print(f'- {a} -> да')
+if a >= 1 and a <= 5:
+      print(f'-{a} -> нет')
+elif a > 5 and a <= 7:
+    print (f'- {a} -> да')
 else:
     print('это не день недели')
